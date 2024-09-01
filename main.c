d505d186b43e75d96f2c1d3f9670afcf2e5ace19
ghccps <167923020+ghccps@users.noreply.github.com>
2024-09-01 22:52:00 +0000

    Adicionado criação da raiz da árvore

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Tree
 {
@@ -7,11 +8,48 @@ typedef struct Tree
     struct Tree *right;
 } Node;
 
+Node *create(int value)
+{
+    struct Tree *newNode = (struct Tree *)malloc(sizeof(struct Tree));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
 int main()
 {
+    int option;
     int value;
 
     Node *root = NULL;
 
+    do
+    {
+        printf("\n1 - Adicionar raiz\n(-1) - sair\n");
+        printf("\nDigite a opção: ");
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            printf("\nDigite o valor a ser adicionado: ");
+            scanf("%d", &value);
+
+            root = create(value);
+            break;
+
+        case -1:
+            printf("\nEncerrando...\n\n");
+            return 0;
+
+        default:
+            printf("\nValor inválido\n");
+            break;
+        }
+
+    } while (option >= 0);
+
+    printf("\n\n\tErro!\n\n");
     return 0;
 }
