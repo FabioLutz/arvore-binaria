efc210e6351274705c0dfea48b5ab988c467691e
ghccps <167923020+ghccps@users.noreply.github.com>
2024-09-01 22:44:35 +0000

    Adicionado estrutura básica da árvore

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct Tree
+{
+    int data;
+    struct Tree *left;
+    struct Tree *right;
+} Node;
+
+int main()
+{
+    int value;
+
+    Node *root = NULL;
+
+    return 0;
+}
